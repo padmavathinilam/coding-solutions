5225a72b7c2bee5dcef352a25b897914764174d8
padmavathinilam <padmavathinilam@gmail.com>
2026-07-03 19:40:37 +0530

    ✅ GSQ07 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-03

---
+++ codechef/medium/GSQ07/solution.cpp
@@ -0,0 +1,5 @@
+/* Write a query to add the below mentioned employee details to the EMPLOYEE table.
+Employee id: 06, Employee Name: Brandon Kim, Department: Operations
+*/
+INSERT INTO employee(Employee_id, Employee_Name, Department)
+VA
