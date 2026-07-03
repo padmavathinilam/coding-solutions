186b3b2218252746d79cbd20f2229a849e5aaef0
padmavathinilam <padmavathinilam@gmail.com>
2026-07-03 22:37:17 +0530

    ✅ GSQ13 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-03

---
+++ codechef/medium/GSQ13/solution.cpp
@@ -0,0 +1,10 @@
+/* Write a query to create a table employee with the mentioned constraints on the columns : 
+employee_id - PRIMARY KEY, 
+employee_Name -UNIQUE, 
+department -NOT NULL CHECK (department IN ('Sales', 'Finance', 'Operations')) */
+CREATE TABLE Employee(
+    employee_id INTEGER PRIMARY KEY,
+    employee_Name TEXT UNIQUE,
+    department TEXT NOT NULL CHECK (department IN('Sales', 'Finance', 'Operations'))
+    
+);
