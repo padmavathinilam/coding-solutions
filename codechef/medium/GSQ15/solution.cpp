f7847eea23839ebcdb7d78462ea0f7eed69288ee
padmavathinilam <padmavathinilam@gmail.com>
2026-07-03 23:03:13 +0530

    ✅ GSQ15 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-03

---
+++ codechef/medium/GSQ15/solution.cpp
@@ -0,0 +1,7 @@
+/* Write a query which does the following
+- Delete the row where the department is Client.
+- Output the entire table.
+*/
+DELETE from employee
+WHERE Department = 'Client';
+SELECT * fro
