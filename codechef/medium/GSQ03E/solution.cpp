d3e5886ad0148a62f651fbe5ae92a4b3aa96b56d
padmavathinilam <padmavathinilam@gmail.com>
2026-07-03 19:32:08 +0530

    ✅ GSQ03E | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-03

---
+++ codechef/medium/GSQ03E/solution.cpp
@@ -0,0 +1,6 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+
+INSERT INTO Employee(employee_id, employee_name, department)
+VALUES
+(4, 'Marcus Garcia', 'Product'),
+(5, 'Samantha Park', 'Hr');
