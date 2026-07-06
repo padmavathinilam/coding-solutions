c6196aa969e8af4a87c2019743eafb5ec7ec69ab
padmavathinilam <padmavathinilam@gmail.com>
2026-07-06 16:21:19 +0530

    ✅ HTPROB43 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-06

---
+++ codechef/medium/HTPROB43/solution.cpp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Movie Review</title>
+  </head>
+  <body>
+    <h1> My Favourite Movie</h1>
+   <h2> Synopsis</h2>
+  </body>
+   <h3>The Hero's</h3>
+</html>
