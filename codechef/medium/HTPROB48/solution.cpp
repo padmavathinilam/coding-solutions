a8a34706c66c77341ccc18902633a469fd9354f2
padmavathinilam <padmavathinilam@gmail.com>
2026-07-06 16:28:58 +0530

    ✅ HTPROB48 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-06

---
+++ codechef/medium/HTPROB48/solution.cpp
@@ -0,0 +1,17 @@
+    <head>
+        <title>
+            My Paragraph Page
+        </title>
+    </head>
+    <body>
+        <h1> My Favorite Activity</h1>
+        <p> I love playing basketball because it's great exercise and fun with friends. The teamwork and competition keep me motivated to improve my skills.
+            
+        </p>
+        <h2>How to Get Started</h2>
+        <p>
+            1.Find a local court or park
+            2.Get a comfortable pair of sneakers
+            3.practice basic dribbling and shooting first.
+        </p>
+    </body>
