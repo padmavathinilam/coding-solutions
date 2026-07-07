4b575518ce0a699ae27dff936741989300c56d00
padmavathinilam <padmavathinilam@gmail.com>
2026-07-07 19:48:06 +0530

    ✅ GSQ30 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-07

---
+++ codechef/medium/GSQ30/solution.cpp
@@ -0,0 +1,6 @@
+/* Write a query based on the following conditions 
+Destination is 'Cairo'
+Entries are ordered by passenger_name in a descending order
+*/
+select * from flights
+where Destination='Cairo' order by p
