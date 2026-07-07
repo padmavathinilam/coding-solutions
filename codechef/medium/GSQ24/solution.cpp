6e252adb264f91833b3e3b88707815b813ac2012
padmavathinilam <padmavathinilam@gmail.com>
2026-07-07 19:34:55 +0530

    ✅ GSQ24 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-07

---
+++ codechef/medium/GSQ24/solution.cpp
@@ -0,0 +1,6 @@
+/* write a query below that returns the rows which meet the following conditions
+- Gender - Female
+- Destination - Cairo */
+select * from Flights
+where gender='Female' and destination = 'Cairo';
+
