29eb64334384f84b86f4ae4377a411ef2483dfde
padmavathinilam <padmavathinilam@gmail.com>
2026-07-07 19:19:06 +0530

    ✅ GSQ20 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-07

---
+++ codechef/medium/GSQ20/solution.cpp
@@ -0,0 +1,6 @@
+/* Write a query to do the following
+- Output all entries from the table
+- Where passenger_name between 'A' and 'D' */
+select * from Flights Where Passenger_name BETWEEN 'A' and 'D'
+
+
