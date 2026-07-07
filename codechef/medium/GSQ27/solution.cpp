dc4e6b5e1a525b61e3a698c1085f0d505e592e5b
padmavathinilam <padmavathinilam@gmail.com>
2026-07-07 19:46:23 +0530

    ✅ GSQ27 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-07

---
+++ codechef/medium/GSQ27/solution.cpp
@@ -0,0 +1,8 @@
+/* Debug this query to meet the following conditons
+- Passenger gender - Female AND
+- Flight destination - Delhi
+*/
+
+select * from flights
+where gender='Female'
+and destination  %i%';
